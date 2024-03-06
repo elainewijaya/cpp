084ad9fc8a14199bc8ef3dd30bc9baacb3bc13ef
Elaine Wijaya <elainew@blackmagicdesign.com>
2024-03-07 00:00:45 +0800

    add class specialization

--- src/template-specialization.cpp
+++ src/template-specialization.cpp
@@ -12,7 +12,6 @@ using namespace std;
   * This is why we have template specialization.
   *
   * ref: https://www.learncpp.com/cpp-tutorial/function-template-specialization/
-  * ref: https://www.learncpp.com/cpp-tutorial/class-template-specialization/
   */
 
 template<typename T> // generic
@@ -32,10 +31,59 @@ void functionSpecializations() {
   //print(A());
 }
 
+/**
+  * Template Specialization - CLASSES
+  *
+  * For classes, we normally think of generic templates that will implicitly instantiate
+  * its members and functions, for example std::vector<T>, which will allocate objects of type
+  * T for each. But sometimes we want the class to do something different, e.g vector<bool> which could
+  * be optimized as a bitset.
+  *
+  * This is NOT recommended but we will demonstrate that it is indeed the case.
+  * A workaround will be to user std::vector<char> or std::deque<bool>, which doesn't have a specialization.
+  * Or, for fixed-size arrays of bits, use std::bitset.
+  *
+  * ref: https://www.learncpp.com/cpp-tutorial/class-template-specialization/
+  * ref: https://cplusplus.com/reference/vector/vector-bool/#google_vignette
+  * ref: http://en.cppreference.com/w/cpp/container/vector_bool
+  */
 
+template <typename T>
+void printFirst(vector<T>& v) {
+  T& first = v[0];
+  cout << first << endl;
+}
 
+template<>
+void printFirst(vector<bool>& v) {
+  vector<bool>::reference first = v[0];
+  cout << first << endl;
+}
+
+template <typename T>
+void printFirstByVal(vector<T>& v) {
+  T first = v[0];
+  cout << first << endl;
+}
+
+void classSpecializations() {
+  vector<char> charVec(10000, 'a');
+  vector<bool> boolVec(10000, true);
+  // doesn't work without template specialization for vector<bool>
+  // error: non-const lvalue reference to type 'bool' cannot bind to a temporary of type 'std::vector<bool>::reference' (aka '__bit_reference<std::vector<bool>>')
+  printFirst(charVec);
+  printFirst(boolVec);
+  // works because assignment of vector<bool>::reference into bool is allowed
+  printFirstByVal(charVec);
+  printFirstByVal(boolVec);
+
+  // error: no member named 'data' in 'std::vector<bool>'
+  // data is not necessarily stored in a contiguous chunk of memory, so we can't access the raw ptr and use ptr arithmetic
+  // boolVec.data();
+}
 
 int main() {
   functionSpecializations();
+  classSpecializations();
   return 0;
 }
